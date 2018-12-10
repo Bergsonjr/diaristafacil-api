c17a39d4325cb8f44d05c0e442581e78871c7ec9
Bergson <bergsonjr@icloud.com>
2018-12-10 09:35:24 -0200

    first steps

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const server = express()
+var express = require('express')
+var app = express()
+
+app.get('/hello', function (req, res) {
+    res.send("Hello World!")
+})
+
+app.post('/hello', function (req, res) {
+    res.send("You just called the post method at '/hello'!\n")
+})
+
+app.use(function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(3000, function () {
+    console.log('Listening on port http://localhost:' + 3000)
+});
